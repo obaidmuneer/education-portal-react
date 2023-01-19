054a5a5618896d0ff9e8cf3fcef4a9112ed4052d
obaidmuneer <obaidmuneer55@gmail.com>
2023-01-20 00:07:28 +0500

    added comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css';
+import { Box } from '@chakra-ui/react'
+import Navbar from './components/nabar';
+import Home from './components/home';
+
 
 function App() {
   return (
-    <div>
-      <h2>Welcome to Sysborg Clone</h2>
-    </div>
-  );
+    <>
+      <Navbar />
+      <Box p={4}>
+        {
+          <Home />
+        }
+      </Box>
+    </>
+
+  )
 }
 
 export default App;
