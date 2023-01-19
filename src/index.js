0d6bd90e27199fa73ee167559e038ccbdbd7e27f
obaidmuneer <obaidmuneer55@gmail.com>
2023-01-20 00:05:33 +0500

    added theme mode

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
+import theme from './theme/theme'
+import { ColorModeScript } from '@chakra-ui/react'
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <ChakraProvider>
       <App />
     </ChakraProvider>
