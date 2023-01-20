5fd9f3ad0bc1e7b153fdf05db78c3981eb162141
obaidmuneer <obaidmuneer55@gmail.com>
2023-01-21 00:00:05 +0500

    input updated

---
+++ src/components/formikInput/index.jsx
@@ -0,0 +1,50 @@
+import { FormControl, FormErrorMessage } from '@chakra-ui/react';
+import {
+    Stack,
+    useColorModeValue,
+    Input,
+    IconButton,
+} from '@chakra-ui/react';
+
+const FormikInput = ({ color, icon, placeHolder, hideBtn, formik, nameLabel }) => {
+    const c = color || 'orange';
+    const bg_c = useColorModeValue(`${c}.400`, `${c}.800`)
+    const sec_c = useColorModeValue('white', 'gray.800')
+
+    return (
+        <>
+            <Stack direction={'row'} >
+                <FormControl isInvalid={formik.errors[nameLabel] && formik.touched[nameLabel]}>
+                    <Input
+                        bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+                        border={0}
+                        _focus={{
+                            bg: 'whiteAlpha.300',
+                        }}
+                        id={nameLabel}
+                        name={nameLabel}
+                        value={formik.values[nameLabel]}
+                        onChange={formik.handleChange}
+                        placeholder={placeHolder}
+                    />
+                    <FormErrorMessage>{formik.errors[nameLabel]}</FormErrorMessage>
+                </FormControl>
+                {
+                    !hideBtn && <IconButton
+                        bg={bg_c}
+                        color={sec_c}
+                        _hover={{
+                            bg: `${c}.600`,
+                        }}
+                        aria-label="Subscribe"
+                        icon={icon}
+                        isLoading={formik.isSubmitting}
+                        type='submit'
+                    />
+                }
+            </Stack>
+        </>
+    )
+}
+
+export default FormikInput
