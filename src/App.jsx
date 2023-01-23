303aa0965acae7782b5042b2745f4d42658e3808
obaidmuneer <obaidmuneer55@gmail.com>
2023-01-23 22:02:41 +0500

    router configured

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,31 @@ import './App.css';
 import Navbar from './components/nabar';
 import Home from './components/home';
 import { Box } from '@chakra-ui/react';
-
+import CDrawer from './components/cDrawer';
+import { Routes, Route } from 'react-router-dom'
+import Signup from './components/signup';
+import Signin from './components/signin';
+import { useContext } from 'react';
+import { GlobalContext } from './context/context';
 
 function App() {
+  const { state, dispatch } = useContext(GlobalContext)
   return (
     <>
       <Navbar />
+      {state.user ? <CDrawer /> : null}
       <Box p={2} >
         {
-          <Home />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/signin' element={<Signin />} />
+          </Routes>
+
+
         }
       </Box>
+
     </>
 
   )
--- src/index.js
+++ src/index.js
@@ -6,17 +6,18 @@ import { ChakraProvider } from '@chakra-ui/react'
 import theme from './theme/theme'
 import { ColorModeScript } from '@chakra-ui/react'
 import ContextProvider from './context/context';
-
-
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <ContextProvider>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </ContextProvider>
+    <BrowserRouter>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <ContextProvider>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </ContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
