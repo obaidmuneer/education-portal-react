7f5783e2f1133633bfec9653e08bafc3f1c2775f
obaidmuneer <obaidmuneer55@gmail.com>
2023-01-24 20:23:38 +0500

    signin

--- src/components/signin/index.jsx
+++ src/components/signin/index.jsx
@@ -11,12 +11,42 @@ import {
     Heading,
     Text,
     useColorModeValue,
+    InputRightElement,
+    InputGroup
 } from '@chakra-ui/react';
+import axios from 'axios';
+import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { GlobalContext } from '../../context/context';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+
 
 export default function Signin() {
+    const { state, dispatch } = useContext(GlobalContext)
+    const [showPassword, setShowPassword] = useState(false);
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const handleSignIn = async (e) => {
+        e.preventDefault()
+        try {
+            const result = await axios.post(`${state.api}users/signin`,
+                { email, password },
+                { withCredentials: true })
+            dispatch({
+                type: 'signin',
+                payload: result.data
+            })
+            navigate('/')
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <Flex
-            minH={'100vh'}
+            // minH={'100v  h'}
             align={'center'}
             justify={'center'}
             bg={useColorModeValue('gray.50', 'gray.800')}>
@@ -31,15 +61,35 @@ export default function Signin() {
                     rounded={'lg'}
                     bg={useColorModeValue('white', 'gray.700')}
                     boxShadow={'lg'}
-                    p={8}>
+                    p={8}
+                    as={'form'}
+                    onSubmit={handleSignIn}
+                >
                     <Stack spacing={4}>
                         <FormControl id="email">
                             <FormLabel>Email address</FormLabel>
-                            <Input type="email" />
+                            <Input type="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                            />
                         </FormControl>
                         <FormControl id="password">
                             <FormLabel>Password</FormLabel>
-                            <Input type="password" />
+                            <InputGroup>
+                                <Input
+                                    type={showPassword ? 'text' : 'password'}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    value={password} />
+                                <InputRightElement h={'full'}>
+                                    <Button
+                                        variant={'ghost'}
+                                        onClick={() =>
+                                            setShowPassword((showPassword) => !showPassword)
+                                        }>
+                                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                                    </Button>
+                                </InputRightElement>
+                            </InputGroup>
                         </FormControl>
                         <Stack spacing={10}>
                             <Stack
@@ -54,7 +104,8 @@ export default function Signin() {
                                 color={'white'}
                                 _hover={{
                                     bg: 'blue.500',
-                                }}>
+                                }}
+                                type={'submit'}>
                                 Sign in
                             </Button>
                         </Stack>
