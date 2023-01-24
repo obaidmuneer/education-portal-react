e2a9c625d327220ce5eb008ace8f83376c255d99
obaidmuneer <obaidmuneer55@gmail.com>
2023-01-24 20:22:59 +0500

    get profile

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,50 @@ import Navbar from './components/nabar';
 import Home from './components/home';
 import { Box } from '@chakra-ui/react';
 import CDrawer from './components/cDrawer';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Signup from './components/signup';
 import Signin from './components/signin';
 import { useContext } from 'react';
 import { GlobalContext } from './context/context';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
   const { state, dispatch } = useContext(GlobalContext)
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await axios.get(`${state.api}users/profile`, {
+          withCredentials: true
+        })
+        // console.log(result);
+        dispatch({
+          type: 'signin',
+          payload: result.data.user
+        })
+      } catch (error) {
+        console.log(error);
+        console.log('please signin to enjoy all our features');
+      }
+    })()
+  }, [])
+
   return (
     <>
       <Navbar />
       {state.user ? <CDrawer /> : null}
-      <Box p={2} >
-        {
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/signin' element={<Signin />} />
-          </Routes>
-
-
-        }
+      <Box p={0} >
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {
+            state?.user ? null : <>
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/signin' element={<Signin />} />
+            </>
+          }
+          <Route path='*' element={<Navigate to={'/'} replace={true} />} />
+        </Routes>
       </Box>
 
     </>
