aaec20465e3b8c429c397b3e225393292905dc33
obaidmuneer <obaidmuneer55@gmail.com>
2023-02-08 18:52:46 +0500

    bug fixed 'err on posting code'

--- src/components/linkForm/index.jsx
+++ src/components/linkForm/index.jsx
@@ -57,41 +57,42 @@ const LinkForm = () => {
 
     return (
         <Box mb={4} width='full'>
-            <Stack as={'form'} onSubmit={formik.handleSubmit} direction={'row'}  >
-                <FormControl isInvalid={formik.errors.link && formik.touched.link}>
-                    <InputGroup>
-                        <Input
-                            bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-                            border={0}
-                            _focus={{
-                                bg: 'whiteAlpha.300',
-                            }}
-                            id={'link'}
-                            name={'link'}
-                            value={formik.values.link}
-                            onChange={formik.handleChange}
-                            placeholder={'Enter Your Link Here'}
+            <Stack direction={'row'}>
+                <Stack as={'form'} width={"full"} onSubmit={formik.handleSubmit} direction={'row'} >
+                    <FormControl isInvalid={formik.errors.link && formik.touched.link}>
+                        <InputGroup>
+                            <Input
+                                bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+                                border={0}
+                                _focus={{
+                                    bg: 'whiteAlpha.300',
+                                }}
+                                id={'link'}
+                                name={'link'}
+                                value={formik.values.link}
+                                onChange={formik.handleChange}
+                                placeholder={'Enter Your Link Here'}
 
-                        />
-                        <InputRightElement>
-                            <CModal isCode={true}  >
-                                <CodeForm />
-                            </CModal>
-                        </InputRightElement>
-                    </InputGroup>
-                    <FormErrorMessage>{formik.errors.link}</FormErrorMessage>
-                </FormControl>
-                <IconButton
-                    bg={`blue.400`}
-                    color={useColorModeValue('white', 'gray.800')}
-                    _hover={{
-                        bg: `blue.600`,
-                    }}
-                    aria-label="add link"
-                    icon={<GoDiffAdded />}
-                    isLoading={formik.isSubmitting}
-                    type='submit'
-                />
+                            />
+                        </InputGroup>
+                        <FormErrorMessage>{formik.errors.link}</FormErrorMessage>
+                    </FormControl>
+
+                    <IconButton
+                        bg={`blue.400`}
+                        color={useColorModeValue('white', 'gray.800')}
+                        _hover={{
+                            bg: `blue.600`,
+                        }}
+                        aria-label="add link"
+                        icon={<GoDiffAdded />}
+                        isLoading={formik.isSubmitting}
+                        type='submit'
+                    />
+                </Stack>
+                <CModal isCode={true}  >
+                    <CodeForm />
+                </CModal>
                 <CModal >
                     <SelectFile />
                 </CModal>
