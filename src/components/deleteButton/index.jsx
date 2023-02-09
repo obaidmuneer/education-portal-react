4e1560d09f445a367c8a0afdd3099b361e1f3bdd
obaidmuneer <obaidmuneer55@gmail.com>
2023-02-09 23:33:34 +0500

    secured deletion

--- src/components/deleteButton/index.jsx
+++ src/components/deleteButton/index.jsx
@@ -1,20 +1,40 @@
-import { IconButton } from '@chakra-ui/react'
-import { FaTrash } from 'react-icons/fa'
+import * as yup from 'yup'
+import { useFormik } from 'formik'
+
 import useDelete from '../../hooks/useDelete'
+import CModal from '../ui-component/CModal'
+import FormikForm from '../formikForm'
+import { FaTrash } from 'react-icons/fa'
+import { BiSend } from 'react-icons/bi'
+
+const validationSchema = yup.object().shape({
+    password: yup.string().required('Please enter password')
+})
 
 const DeleteButton = ({ docId, i }) => {
-    const { handleDelete, isLoading } = useDelete()
+    const { handleDelete } = useDelete()
+    const formik = useFormik({
+        initialValues: {
+            password: ''
+        },
+        validationSchema: validationSchema,
+        onSubmit: async (values, actions) => {
+            await handleDelete(docId, i, values.password)
+            actions.setSubmitting(false)
+        }
+    })
 
-    const buttonProps = {
-        icon: <FaTrash />,
-        isRound: true,
-        'aria-label': 'delete',
-    }
-
-    return <IconButton
-        onClick={() => handleDelete(docId, i)}
-        {...buttonProps}
-        isLoading={isLoading} />
+    return <>
+        <CModal icon={FaTrash} label={'Delete'} header={'Please Enter Password'} >
+            <FormikForm
+                formik={formik}
+                color={'blue'}
+                nameLabel={'password'}
+                placeHolder={'Enter Password'}
+                type='password'
+                icon={<BiSend />} />
+        </CModal>
+    </>
 }
 
 export default DeleteButton
